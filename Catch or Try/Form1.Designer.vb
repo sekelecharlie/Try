669bac13d3e7b525c68be8e6a019f0193362389a
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-08 11:57:23 +0200

    Add project files.

---
+++ Catch or Try/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnPress = New Button()
+        Imput = New Label()
+        txtImput = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnPress
+        ' 
+        btnPress.Location = New Point(285, 160)
+        btnPress.Name = "btnPress"
+        btnPress.Size = New Size(129, 50)
+        btnPress.TabIndex = 0
+        btnPress.Text = "Press"
+        btnPress.UseVisualStyleBackColor = True
+        ' 
+        ' Imput
+        ' 
+        Imput.AutoSize = True
+        Imput.Location = New Point(181, 107)
+        Imput.Name = "Imput"
+        Imput.Size = New Size(60, 25)
+        Imput.TabIndex = 1
+        Imput.Text = "Imput"
+        ' 
+        ' txtImput
+        ' 
+        txtImput.Location = New Point(285, 107)
+        txtImput.Name = "txtImput"
+        txtImput.Size = New Size(150, 31)
+        txtImput.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtImput)
+        Controls.Add(Imput)
+        Controls.Add(btnPress)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnPress As Button
+    Friend WithEvents Imput As Label
+    Friend WithEvents txtImput As TextBox
+End Class
---
+++ Catch or Try/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnPress_Click(sender As Object, e As EventArgs) Handles btnPress.Click
+        Dim isNumber As String
+        isNumber = txtImput.Text
+
+        Try
+
+            Dim numericValue As Double
+            numericValue = CObj(isNumber)
+            MsgBox("That is a Valid Number You have entered " & numericValue)
+
+
+        Catch
+
+            MsgBox("Incorrect: " & isNumber & "  is not a number!!!")
+
+        End Try
+    End Sub
+End Class
